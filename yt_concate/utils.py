98397d7d9dcc40c25de9946b1dc0c2b38f97df0d
SI-YU <bito8651@gmail.com>
2022-01-13 01:08:03 +0800

    Finished editing videos and producing output file

--- yt_concate/utils.py
+++ yt_concate/utils.py
@@ -3,6 +3,7 @@ import os
 from yt_concate.settings import DOWNLOADS_DIR
 from yt_concate.settings import VIDEOS_DIR
 from yt_concate.settings import CAPTIONS_DIR
+from yt_concate.settings import OUTPUT_DIR
 
 
 class Utils:
@@ -22,12 +23,19 @@ class Utils:
         os.makedirs(DOWNLOADS_DIR, exist_ok=True)
         os.makedirs(VIDEOS_DIR, exist_ok=True)
         os.makedirs(CAPTIONS_DIR, exist_ok=True)
+        os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     @staticmethod
     def captions_file_exists(yt):
         filepath = yt.caption_filepath
         return os.path.exists(filepath) and os.path.getsize(filepath) > 0
+
     @staticmethod
     def video_file_exists(yt):
         filepath = yt.video_filepath
         return os.path.exists(filepath) and os.path.getsize(filepath) > 0
+
+    @staticmethod
+    def get_output_filepath(channel_id, search_word):
+        filename = f'{channel_id}_{search_word}.mp4'
+        return os.path.join(OUTPUT_DIR, filename)
